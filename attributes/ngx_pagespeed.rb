17274a3a1744fac6fb84d2124a4296149980bdd9
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-09-22 21:06:20 +0200

    track development from dop_nginx;

--- attributes/ngx_pagespeed.rb
+++ attributes/ngx_pagespeed.rb
@@ -4,7 +4,7 @@
 #
 # Author:: Achim Rosenhagen (<a.rosenhagen@ffuenf.de>)
 #
-# Copyright 2013, Achim Rosenhagen
+# Copyright 2013-2014, Achim Rosenhagen
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,15 +19,43 @@
 # limitations under the License.
 #
 
-default['nginx']['ngx_pagespeed']['version']  = "release-1.6.29.5-beta"
-default['nginx']['ngx_pagespeed']['url']      = "https://github.com/pagespeed/ngx_pagespeed/archive/#{node['nginx']['ngx_pagespeed']['version']}.tar.gz"
-default['nginx']['ngx_pagespeed']['src']['cookbook'] = "nginx"
-default['nginx']['ngx_pagespeed']['src']['file'] = nil
+default['nginx']['ngx_pagespeed']['version'] = '1.9.32.1-beta'
+default['nginx']['ngx_pagespeed']['url'] = "https://codeload.github.com/pagespeed/ngx_pagespeed/tar.gz/v#{node['nginx']['ngx_pagespeed']['version']}"
+default['nginx']['ngx_pagespeed']['checksum'] = '072d4ce236a07c401f8a39a7929daa15fd934c76b97d00ab74f0b77acb126dacb65b3b334b80118c5a68698ac35b1914a726b78aa8b57b66ba9a1558ddde7277'
 
-default['nginx']['ngx_pagespeed']['psol']['version']  = "1.5.27.3"
+default['nginx']['ngx_pagespeed']['psol']['version'] = '1.9.32.1'
 default['nginx']['ngx_pagespeed']['psol']['url'] = "https://dl.google.com/dl/page-speed/psol/#{node['nginx']['ngx_pagespeed']['psol']['version']}.tar.gz"
-default['nginx']['ngx_pagespeed']['psol']['src']['cookbook'] = "nginx"
-default['nginx']['ngx_pagespeed']['psol']['src']['file'] = nil
+default['nginx']['ngx_pagespeed']['checksum'] = 'b43820eb28353cd0b22d8c2142e1fcedb30ce8d5abdb940faeb88cfafb793bc3a2354b21797b451deb5a9edf406c6c865ce564ea25b22cdb5ff676b928d4619d'
 
-default['nginx']['ngx_pagespeed']['FileCachePath'] = "/tmp/ngx_pagespeed_cache"
-default['nginx']['ngx_pagespeed']['EnableFilters'] = "collapse_whitespace,combine_css,combine_javascript,remove_comments,sprite_images"
+default['nginx']['ngx_pagespeed']['cache-url'] = '/var/ngx_pagespeed_cache'
+default['nginx']['ngx_pagespeed']['cache-size'] = '512M'
+default['nginx']['ngx_pagespeed']['enabled'] = 'on'
+default['nginx']['ngx_pagespeed']['canonicalize_javascript_libraries'] = false
+default['nginx']['ngx_pagespeed']['FileCachePath'] = node['nginx']['ngx_pagespeed']['cache-url']
+default['nginx']['ngx_pagespeed']['CacheFlushFilename'] = 'cache.flush'
+default['nginx']['ngx_pagespeed']['EnableFilters'] = 'collapse_whitespace,combine_css,combine_javascript,remove_comments,sprite_images,extend_cache'
+default['nginx']['ngx_pagespeed']['DisableFilters'] = 'defer_javascript'
+
+default['nginx']['ngx_pagespeed']['Statistics'] = 'on'
+default['nginx']['ngx_pagespeed']['StatisticsLogging'] = 'on'
+default['nginx']['ngx_pagespeed']['LogDir'] = '/var/log/nginx/pagespeed'
+default['nginx']['ngx_pagespeed']['StatisticsLoggingIntervalMs'] = 60_000
+default['nginx']['ngx_pagespeed']['StatisticsLoggingMaxFileSizeKb'] = 1_024
+default['nginx']['ngx_pagespeed']['MessageBufferSize'] = 100_000
+
+default['nginx']['ngx_pagespeed']['AdminPath'] = '/ngx_pagespeed_admin'
+default['nginx']['ngx_pagespeed']['GlobalAdminPath'] = '/ngx_pagespeed_global_admin'
+default['nginx']['ngx_pagespeed']['StatisticsPath'] = '/ngx_pagespeed_statistics'
+default['nginx']['ngx_pagespeed']['GlobalStatisticsPath'] = '/ngx_pagespeed_global_statistics'
+default['nginx']['ngx_pagespeed']['MessagesPath'] = '/ngx_pagespeed_message'
+default['nginx']['ngx_pagespeed']['ConsolePath'] = '/ngx_pagespeed_console'
+
+default['nginx']['ngx_pagespeed']['FileCacheSizeKb'] = 102_400
+default['nginx']['ngx_pagespeed']['FileCacheCleanIntervalMs'] = 600_000
+default['nginx']['ngx_pagespeed']['FileCacheInodeLimit'] = 500_000
+default['nginx']['ngx_pagespeed']['LRUCacheKbPerProcess'] = 8_192
+default['nginx']['ngx_pagespeed']['LRUCacheByteLimit'] = 16_384
+default['nginx']['ngx_pagespeed']['CacheFlushPollIntervalSec'] = 600_000
+default['nginx']['ngx_pagespeed']['DownstreamCachePurgeMethod'] = 'PURGE'
+default['nginx']['ngx_pagespeed']['DownstreamCacheRewrittenPercentageThreshold'] = 95
+default['nginx']['ngx_pagespeed']['RetainComment'] = 'esi*'
--- recipes/http_ngx_pagespeed_module.rb
+++ recipes/http_ngx_pagespeed_module.rb
@@ -4,7 +4,7 @@
 #
 # Author:: Achim Rosenhagen (<a.rosenhagen@ffuenf.de>)
 #
-# Copyright 2013, Achim Rosenhagen
+# Copyright 2013-2014, Achim Rosenhagen
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,10 +19,10 @@
 # limitations under the License.
 #
 
-package "build-essential"
-package "zlib1g-dev"
-package "libpcre3"
-package "libpcre3-dev"
+package 'build-essential'
+package 'zlib1g-dev'
+package 'libpcre3'
+package 'libpcre3-dev'
 
 directory node['nginx']['ngx_pagespeed']['FileCachePath'] do
   owner node['nginx']['user']
@@ -30,71 +30,79 @@ directory node['nginx']['ngx_pagespeed']['FileCachePath'] do
   action :create
 end
 
-ngx_pagespeed_src_filename = ::File.basename(node['nginx']['ngx_pagespeed']['url'])
-ngx_pagespeed_src_filepath = "#{Chef::Config['file_cache_path']}/#{ngx_pagespeed_src_filename}"
-ngx_pagespeed_extract_path = "#{Chef::Config['file_cache_path']}"
-
-remote_file ngx_pagespeed_src_filepath do
-  source node['nginx']['ngx_pagespeed']['url']
-  owner "root"
-  group "root"
-  mode 00644
-  not_if { node['nginx']['ngx_pagespeed']['src']['file'] }
+directory node['nginx']['ngx_pagespeed']['LogDir'] do
+  owner node['nginx']['user']
+  group node['nginx']['user']
+  action :create
 end
 
-cookbook_file ngx_pagespeed_src_filepath do
-  cookbook node['nginx']['ngx_pagespeed']['src']['cookbook']
-  owner "root"
-  group "root"
-  mode 00644
-  only_if { node['nginx']['ngx_pagespeed']['src']['file'] }
+mount node['nginx']['ngx_pagespeed']['FileCachePath'] do
+  device 'tmpfs'
+  fstype 'tmpfs'
+  options ["size=#{node['nginx']['ngx_pagespeed']['cache-size']}", 'mode=1733', 'noatime', 'noexec', 'nosuid', 'nodev']
+  dump 0
+  pass 0
+  action [:enable, :mount]
+  supports [remount: true]
 end
 
-bash "extract_ngx_pagespeed" do
-  cwd ::File.dirname(ngx_pagespeed_src_filepath)
-  code <<-EOH
-    mkdir -p #{ngx_pagespeed_extract_path}
-    tar xzf #{ngx_pagespeed_src_filename} -C #{ngx_pagespeed_extract_path}
-  EOH
+ngx_pagespeed_src_filepath = "#{Chef::Config['file_cache_path']}/ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']}"
+ngx_pagespeed_extract_path = Chef::Config['file_cache_path']
+
+unless ::File.exist?(ngx_pagespeed_src_filepath)
+  remote_file ngx_pagespeed_src_filepath do
+    source node['nginx']['ngx_pagespeed']['url']
+    owner 'root'
+    group 'root'
+    mode 00644
+  end
+  bash 'extract_ngx_pagespeed' do
+    cwd ::File.dirname(ngx_pagespeed_src_filepath)
+    code <<-EOH
+      mkdir -p #{ngx_pagespeed_extract_path}
+      tar xzf ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']} -C #{ngx_pagespeed_extract_path}
+    EOH
+  end
+  bash 'canonicalize_javascript_libraries' do
+    cwd ::File.dirname(ngx_pagespeed_src_filepath)
+    code <<-EOH
+      #{ngx_pagespeed_extract_path}/scripts/pagespeed_libraries_generator.sh > #{node['nginx']['dir']}/pagespeed_libraries.conf
+    EOH
+    only_if { node['nginx']['ngx_pagespeed']['canonicalize_javascript_libraries'] }
+  end
 end
 
 ngx_psol_src_filename = ::File.basename(node['nginx']['ngx_pagespeed']['psol']['url'])
 ngx_psol_src_filepath = "#{Chef::Config['file_cache_path']}/#{ngx_psol_src_filename}"
 ngx_psol_extract_path = "#{ngx_pagespeed_extract_path}/ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']}"
 
-remote_file ngx_psol_src_filepath do
-  source node['nginx']['ngx_pagespeed']['psol']['url']
-  owner "root"
-  group "root"
-  mode 00644
-  not_if { node['nginx']['ngx_pagespeed']['psol']['src']['file'] }
-end
+unless ::File.exist?(ngx_psol_src_filepath)
+  remote_file ngx_psol_src_filepath do
+    source node['nginx']['ngx_pagespeed']['psol']['url']
+    owner 'root'
+    group 'root'
+    mode 00644
+  end
 
-cookbook_file ngx_psol_src_filepath do
-  cookbook node['nginx']['ngx_pagespeed']['psol']['src']['cookbook']
-  owner "root"
-  group "root"
-  mode 00644
-  only_if { node['nginx']['ngx_pagespeed']['psol']['src']['file'] }
-end
-
-bash "extract_psol" do
-  cwd ::File.dirname(ngx_psol_src_filepath)
-  code <<-EOH
-    tar xzf #{ngx_psol_src_filename} -C #{ngx_psol_extract_path}
-  EOH
+  bash 'extract_psol' do
+    cwd ::File.dirname(ngx_psol_src_filepath)
+    code <<-EOH
+      tar xzf #{ngx_psol_src_filename} -C #{ngx_psol_extract_path}
+    EOH
+  end
 end
 
 template "#{node['nginx']['dir']}/conf.d/ngx_pagespeed.conf" do
-  source "modules/ngx_pagespeed.conf.erb"
-  owner "root"
-  group "root"
+  source 'ngx_pagespeed.conf.erb'
+  cookbook 'dop_nginx'
+  owner 'root'
+  group 'root'
   mode 00644
   variables(
-    :params => node['nginx']['ngx_pagespeed']
+    params: node['nginx']['ngx_pagespeed']
   )
-  notifies :reload, "service[nginx]"
+  notifies :reload, 'service[nginx]', :delayed
 end
 
 node.run_state['nginx_configure_flags'] =
-  node.run_state['nginx_configure_flags'] | ["--add-module=#{ngx_pagespeed_extract_path}/ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']}"]
+  node.run_state['nginx_configure_flags'] | ["--add-module=#{Chef::Config['file_cache_path']}/ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']}"]
