e5c3ddaea2d8f8787b4708a3a06d36e1dd07db76
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-09-22 23:27:31 +0200

    fix lib_url destination;

--- attributes/geoip.rb
+++ attributes/geoip.rb
@@ -8,5 +8,5 @@ default['nginx']['geoip']['country_dat_checksum'] = '6802f06eb4225dfb1f22e186f55
 default['nginx']['geoip']['city_dat_url'] = 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz'
 default['nginx']['geoip']['city_dat_checksum'] = '0e40ff174d2281dbd76fea11654d27d4d944a35cca44e2a235642668119b1aeb'
 default['nginx']['geoip']['lib_version'] = '1.6.2'
-default['nginx']['geoip']['lib_url'] = "http://geolite.maxmind.com/download/geoip/api/c/GeoIP-#{node['nginx']['geoip']['lib_version']}.tar.gz"
+default['nginx']['geoip']['lib_url'] = "https://github.com/maxmind/geoip-api-c/releases/download/v1.6.2/GeoIP-#{node['nginx']['geoip']['lib_version']}.tar.gz"
 default['nginx']['geoip']['lib_checksum'] = '8ef059ee3680b39db967248331ec41188f4b45f86a4a77e39247ff41b61efd7c'
