8193102b23401ab41d4087ba5bf8a84b2b1d53f7
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2016-03-24 23:37:14 +0100

    update ngx_pagespeed version set DownstreamCacheRebeaconingKey for proper use with varnish in front conditionally add Disallow directive

--- attributes/pagespeed.rb
+++ attributes/pagespeed.rb
@@ -2,13 +2,13 @@
 # Cookbook Name:: nginx
 # Recipe:: pagespeed_module
 #
-default['nginx']['ngx_pagespeed']['version'] = '1.10.33.4-beta'
+default['nginx']['ngx_pagespeed']['version'] = '1.10.33.5-beta'
 default['nginx']['ngx_pagespeed']['url'] = "https://codeload.github.com/pagespeed/ngx_pagespeed/tar.gz/v#{node['nginx']['ngx_pagespeed']['version']}"
-default['nginx']['ngx_pagespeed']['checksum'] = '4843e069669bdb040b5a47975e56b607da92cc4fcd5a91c985b8cc303b2ea6a6'
+default['nginx']['ngx_pagespeed']['checksum'] = 'fe6211f9f2c1aa7bad852a2ccdf85a1384bf331c12c742f163593a6ccc49091b'
 
-default['nginx']['ngx_pagespeed']['psol']['version'] = '1.10.33.4'
+default['nginx']['ngx_pagespeed']['psol']['version'] = '1.10.33.5'
 default['nginx']['ngx_pagespeed']['psol']['url'] = "https://dl.google.com/dl/page-speed/psol/#{node['nginx']['ngx_pagespeed']['psol']['version']}.tar.gz"
-default['nginx']['ngx_pagespeed']['checksum'] = '243df699210137f90199c11394d28f0eafc5eeb7e8ad981d3381794ab990a98d'
+default['nginx']['ngx_pagespeed']['checksum'] = '5b2ede0f53281a1486b984937eb261c1a9a013b6b161c7663cc8e953e5b6e2ff'
 
 default['nginx']['ngx_pagespeed']['cache-url'] = '/var/ngx_pagespeed_cache'
 default['nginx']['ngx_pagespeed']['cache-size'] = '512M'
@@ -41,6 +41,6 @@ default['nginx']['ngx_pagespeed']['LRUCacheKbPerProcess'] = 8_192
 default['nginx']['ngx_pagespeed']['LRUCacheByteLimit'] = 16_384
 default['nginx']['ngx_pagespeed']['CacheFlushPollIntervalSec'] = 600_000
 default['nginx']['ngx_pagespeed']['DownstreamCachePurgeMethod'] = 'PURGE'
+default['nginx']['ngx_pagespeed']['DownstreamCacheRebeaconingKey'] = 'vfjehtsngihw3f8e'
 default['nginx']['ngx_pagespeed']['DownstreamCacheRewrittenPercentageThreshold'] = 95
 default['nginx']['ngx_pagespeed']['RetainComment'] = 'esi*'
-default['nginx']['ngx_pagespeed']['Disallow'] = ''
