d43524b8b7684da4a3f281ef053b134c5d830089
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2016-03-25 00:22:10 +0100

    fix canonicalize_javascript_libraries

--- recipes/http_ngx_pagespeed_module.rb
+++ recipes/http_ngx_pagespeed_module.rb
@@ -43,13 +43,13 @@ unless ::File.exist?(ngx_pagespeed_src_filepath)
       tar xzf #{ngx_pagespeed_src_filepath} --no-same-owner -C #{ngx_pagespeed_extract_path}
     EOH
   end
-  bash 'canonicalize_javascript_libraries' do
-    cwd ::File.dirname(ngx_pagespeed_src_filepath)
-    code <<-EOH
-      #{ngx_pagespeed_extract_path}/ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']}/scripts/pagespeed_libraries_generator.sh > #{node['nginx']['dir']}/pagespeed_libraries.conf
-    EOH
-    only_if { node['nginx']['ngx_pagespeed']['canonicalize_javascript_libraries'] }
-  end
+end
+bash 'canonicalize_javascript_libraries' do
+  cwd ::File.dirname(ngx_pagespeed_src_filepath)
+  code <<-EOH
+    #{ngx_pagespeed_extract_path}/ngx_pagespeed-#{node['nginx']['ngx_pagespeed']['version']}/scripts/pagespeed_libraries_generator.sh > #{node['nginx']['dir']}/pagespeed_libraries.conf
+  EOH
+  only_if { node['nginx']['ngx_pagespeed']['canonicalize_javascript_libraries'] }
 end
 
 ngx_psol_src_filename = ::File.basename(node['nginx']['ngx_pagespeed']['psol']['url'])
